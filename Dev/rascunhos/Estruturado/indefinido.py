482858dff11fdddb5f6c6f51cda5a903426c25be
Diogo <diogo.nogueira.santos@outlook.com>
2023-08-05 09:00:43 -0300

    incluindo csv para teste de biblioteca seaborn

---
+++ Dev/rascunhos/Estruturado/indefinido.py
@@ -0,0 +1,16 @@
+import getpass
+from typing import List
+
+Usuario = List[str]
+senha = List[int]
+
+print('Olá seja bem-vindo a sua carteira virutal!')
+
+verificacao_usuario = str(input('Possui registro em nosso sistema? [S/N]')).strip().upper()
+
+if verificacao_usuario in 'S':
+    pesquisa_usuario = str(input('Por favor digite o seu nome de usuário: '))
+    if pesquisa_usuario in Usuario:
+        print(f'Seja muito bem vindo(a)! {pesquisa_usuario in Usuario}')
+    elif pesquisa_usuario not in Usuario[:]:
+        print('Usuário não encontrado!')
